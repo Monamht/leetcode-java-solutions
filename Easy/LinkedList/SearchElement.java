14c3d5ff88ab4d08b3a8c2da5a25429a816a5022
Monisha Mahato <146937229+Monamht@users.noreply.github.com>
2026-05-12 00:36:55 +0530

    Document search element approach in SearchElement.java
    Added problem statement and approach for searching an element in a linked list.

--- Easy/LinkedList/SearchElement.java
+++ Easy/LinkedList/SearchElement.java
@@ -1,6 +1,13 @@
 /**Problem Statement: Given the head of a linked list and an integer value, find out whether the integer is present in the linked list or not.
 Return true if it is present, or else return false.
 
+#APPROACH
+1.Initialise a temporary pointer to traverse the entire list.
+2.During the traversal, check if the data on the current node matches the specified value. If no match is found, move to the next node.
+3.Continue this traversal until either fast (or next node of fast) reaches null or both the pointers, slow and fast, meet.
+4.At any moment, if the data of the node matches with the val, stop and return true.
+5.If the temporary pointer reaches null without finding the required value, return false.
+
 Time Complexity: O(N), we traverse the entire linked list once in worst case to search for the required value.
 Space Complexity: O(1) , we use a constant amount of additional space, regardless of the linked list's length to search for an element.**/
 
