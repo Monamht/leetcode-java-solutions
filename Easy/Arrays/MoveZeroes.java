3d1c5be11a6ccfcdf8a916c905157111a45f1342
Monisha Mahato <146937229+Monamht@users.noreply.github.com>
2026-09-25 12:05:58 +0530

    Add moveZeroes method to shift zeros in array
    Implemented a method to move all zeros in the array to the end while maintaining the order of non-zero elements.

---
+++ Easy/Arrays/MoveZeroes.java
@@ -0,0 +1,45 @@
+/**
+Move Zeros to End
+Given an integer array nums, move all the 0's to the end of the array. The relative order of the other elements must remain the same.
+
+This must be done in place, without making a copy of the array.
+
+Example 1:
+Input: nums = [0, 1, 4, 0, 5, 2]
+Output: [1, 4, 5, 2, 0, 0]
+Explanation:
+Both the zeroes are moved to the end and the order of the other elements stay the same
+
+Example 2:
+Input: nums = [0, 0, 0, 1, 3, -2]
+Output: [1, 3, -2, 0, 0, 0]
+Explanation:
+All 3 zeroes are moved to the end and the order of the other elements stay the same
+
+**/
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j=-1;
+        //find zero at the begin 
+        for(int i=0 ; i<nums.length;i++){
+            if(nums[i]==0){
+                j=i;
+                break;
+            }
+        }
+        //non-zero not found 
+        if(j==-1){
+                return;
+        }
+        // if zero found at any place swapping 
+        for(int i=j+1 ; i<nums.length; i++){
+            if(nums[i]!=0){
+                int temp = nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                j++;
+            }
+        }
+        
+    }
+}
